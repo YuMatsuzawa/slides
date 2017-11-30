9807ccfd5066ac5a8ee7780d66fe81736b83e829
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2017-11-30 17:46:20 +0900

    [master] Relative path

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -eu
 mdfile=$1
 dirname=$(dirname "$mdfile")
-reveal-md --theme night --css slides.css --static docs "$mdfile" && mv docs/index.html "docs/${dirname}.html"
+reveal-md --theme night --css ./slides.css --static docs "$mdfile" && mv docs/index.html "docs/${dirname}.html"
