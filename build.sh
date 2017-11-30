0ff66cc109494142e5a1a71eecf9b056a22adc0e
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2017-11-30 17:37:58 +0900

    [master] Update scripts

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -eu
 mdfile=$1
 dirname=$(dirname "$mdfile")
-reveal-md --theme night --css slides.css --static _site "$mdfile" && mv _site/index.html "_site/${dirname}.html"
+reveal-md --theme night --css slides.css --static docs "$mdfile" && mv _site/index.html "_site/${dirname}.html"
